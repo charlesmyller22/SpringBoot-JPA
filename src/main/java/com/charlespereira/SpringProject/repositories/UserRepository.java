f1d561f315bdeeea29c3d0843a12f92fe803d443
Charles Pereira <charlesmyller@hotmail.com>
2023-03-05 14:20:44 +0100

    Service Layer, component registration

--- src/main/java/com/charlespereira/SpringProject/repositories/UserRepository.java
+++ src/main/java/com/charlespereira/SpringProject/repositories/UserRepository.java
@@ -1,7 +1,5 @@
 package com.charlespereira.SpringProject.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.charlespereira.SpringProject.entities.User;
